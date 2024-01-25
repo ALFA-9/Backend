144ec3b502ffbcc8af4e72b698f6c2cc88370648
Den2605 <rezerv50@yandex.ru>
2024-01-25 21:00:08 +0300

    Fix import data

--- backend/users/management/commands/employee_csv.py
+++ backend/users/management/commands/employee_csv.py
@@ -13,37 +13,34 @@ class Command(BaseCommand):
             reader_object = csv.reader(f, delimiter=",")
             next(reader_object, None)
             for row in reader_object:
-                user_id = row[0]
+                user_id = row[1]
                 user = User.objects.get(id=user_id)
-                grade_id = row[6]
+                grade_id = row[7]
                 grade = Grade.objects.get(id=grade_id)
-                post_id = row[7]
+                post_id = row[8]
                 post = Post.objects.get(id=post_id)
-                department_id = row[8]
+                department_id = row[9]
                 department = Department.objects.get(id=department_id)
                 obj = Employee(
+                    id=row[0],
                     user=user,
-                    last_name=row[1],
-                    first_name=row[2],
-                    patronymic=row[3],
-                    phone=row[5],
+                    last_name=row[2],
+                    first_name=row[3],
+                    patronymic=row[4],
+                    phone=row[6],
                     grade=grade,
                     post=post,
                     department=department,
                 )
                 obj.save()
 
-                # Обновление employee через related_name User
+                # Обновление User employee через related_name
                 user.user = obj
                 user.save()
 
-                # # Загрузка руководителей и создание связей
-                # director_id = row[9]
-                # if director_id:
-                #     # director, created = Employee.objects.get_or_create(
-                #     # user=director_name
-                #     # )
-                #     # Сохраняем связь между ID сотрудника и руководителя
-                #     # directors[row[0]] = director
-                #     obj.director = director_id
-                #     obj.save()
+                # Загрузка руководителей и создание связей
+                director_id = row[10]
+                if director_id:
+                    director = Employee.objects.get(id=director_id)
+                    obj.director = director
+                    obj.save()
