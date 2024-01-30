9f042ae8bf44565c4e709f6aa43a0dc92edaa739
Den2605 <rezerv50@yandex.ru>
2024-01-30 18:04:00 +0300

    Added comments to tasks

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -28,6 +28,7 @@ class ControlSerializer(serializers.ModelSerializer):
 class TaskGetSerializer(serializers.ModelSerializer):
     """Сереализатор задач."""
 
+    comments = serializers.SerializerMethodField()
     type = TypeSerializer()
     control = ControlSerializer()
 
@@ -44,12 +45,18 @@ class TaskGetSerializer(serializers.ModelSerializer):
             "control",
             "date_start",
             "date_end",
+            "comments",
         )
 
         extra_kwargs = {
             "date_start": {"input_formats": ["%Y-%m-%d", "%d.%m.%Y"]},
             "date_end": {"input_formats": ["%Y-%m-%d", "%d.%m.%Y"]},
         }
+
+    def get_comments(self, obj):
+        """Сообщения."""
+        comments = obj.comment_task.all()
+        return CommentSerializer(comments, many=True).data
 
     def to_representation(self, instance):
         instance.date_start = instance.date_start.strftime("%d.%m.%Y")
