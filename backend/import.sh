e095eb0b7e576c815b5f012f989bc21f870aa90a
Aleksey <129634706+aleksey2299-1@users.noreply.github.com>
2024-02-05 02:17:23 +0300

    Moved impost.sh into docker

---
+++ backend/import.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+python manage.py csvimport -f data/post.csv -m post
+python manage.py csvimport -f data/grade.csv -m grade
+python manage.py csvimport -f data/department.csv -m department
+python manage.py csvimport -f data/employee.csv -m employee
+python manage.py csvimport -f data/control.csv -m control
+python manage.py csvimport -f data/type.csv -m type
+python manage.py csvimport -f data/idp.csv -m idp
+python manage.py csvimport -f data/task.csv -m task
+python manage.py csvimport -f data/comment.csv -m comment
